131f3226aa5714da5131ceb2b0df63fee3b8f4be
DerJurgen <124790211+DerJurgen@users.noreply.github.com>
2023-04-26 14:12:41 +0300

    Add files via upload
    Shared file for school project

---
+++ Cash Register/Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace Cash_Register
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(300, 513);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 120);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Button 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 318F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 190F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 207F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.listBox2, 3, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(678, 413);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(538, 100);
+            this.tableLayoutPanel2.TabIndex = 0;
+            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(144, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "TOTAL:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(334, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(166, 54);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(678, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(337, 379);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(306, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Coffee";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(427, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 53);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Tea";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(549, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(114, 53);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Soft Drink";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(306, 71);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(115, 52);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Lunch 1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(427, 71);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(116, 52);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Lunch 2";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(549, 71);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(114, 52);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Lunch 3";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(306, 129);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(115, 54);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Pastry 1";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(427, 129);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(116, 54);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Pastry 2";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(549, 129);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(114, 54);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Pastry 3";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(306, 189);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(115, 53);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "Pastry 4";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(427, 189);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(116, 53);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "Pastry 5";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(549, 189);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(114, 53);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "Pastry 6";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(549, 248);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(114, 54);
+            this.button14.TabIndex = 14;
+            this.button14.Text = "Dessert 3";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(427, 248);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(116, 54);
+            this.button15.TabIndex = 15;
+            this.button15.Text = "Dessert 2";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(306, 248);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(115, 54);
+            this.button16.TabIndex = 16;
+            this.button16.Text = "Dessert 1\r\n\r\n";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(306, 390);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(115, 53);
+            this.button17.TabIndex = 17;
+            this.button17.Text = "Clear";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(427, 390);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(116, 53);
+            this.button18.TabIndex = 18;
+            this.button18.Text = "Voucher";
+            this.button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(549, 390);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(114, 53);
+            this.button19.TabIndex = 19;
+            this.button19.Text = "TOTAL";
+            this.button19.UseVisualStyleBackColor = true;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 25;
+            this.listBox3.Location = new System.Drawing.Point(1036, 12);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(142, 379);
+            this.listBox3.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1218, 507);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Cash register 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Button button1;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private ListBox listBox3;
+    }
+}
---
+++ Cash Register/Form1.cs
@@ -0,0 +1,143 @@
+namespace Cash_Register
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Coffee");
+            listBox3.Items.Add("2.5€");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Pastry 3");
+            listBox3.Items.Add("2.0€");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Pastry 5");
+            listBox3.Items.Add("2.5€");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Lunch 1");
+            listBox3.Items.Add("7€");
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox3.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Tea");
+            listBox3.Items.Add("2.0€");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Soft Drink");
+            listBox3.Items.Add("2.5€");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Lunch 2");
+            listBox3.Items.Add("8.5€");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Lunch 3");
+            listBox3.Items.Add("9€");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Pastry 1");
+            listBox3.Items.Add("2.5€");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Pastry 2");
+            listBox3.Items.Add("4.0€");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Pastry 4");
+            listBox3.Items.Add("2.5€");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Pastry 6");
+            listBox3.Items.Add("2.8€");
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Dessert 1");
+            listBox3.Items.Add("5.0€");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Dessert 2");
+            listBox3.Items.Add("6.0€");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Dessert 3");
+            listBox3.Items.Add("4.0€");
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            {
+            }
+        }
+    }
+}
